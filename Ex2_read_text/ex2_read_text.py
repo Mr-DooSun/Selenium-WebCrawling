7fa1fc6cb84c34e684f722988e3ecec901813be5
Mr-DooSun <mrdoosun@gmail.com>
2021-12-21 23:31:20 +0900

    xpath 변수 추가 및 text 출력

--- Ex2_read_text/ex2_read_text.py
+++ Ex2_read_text/ex2_read_text.py
@@ -15,6 +15,12 @@ driver = webdriver.Chrome(options=options)
 # 브라우저에 URL 호출하기
 driver.get(url='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%82%A0%EC%94%A8')
 
+# 이전에 복사한 xpath값을 아래 함수에 붙여넣기 해줍니다.
+# driver.find_element_by_xpath(XPATH)
+temperature_xpath = driver.find_element_by_xpath('//*[@id="main_pack"]/section[1]/div[1]/div[2]/div[1]/div[1]/div/div[2]/div/div[1]/div[2]/strong')
+temperature = temperature_xpath.text
+print(temperature)
+
 # 브라우저 탭 닫기
 driver.close()
 # 브라우저 종료하기 (탭 모두 종료)
